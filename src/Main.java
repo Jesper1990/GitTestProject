530dee8d48c6974f556d8145c6333ce27041b975
Jesper Lundbäck <68069660+Jesper1990@users.noreply.github.com>
2020-10-21 17:36:35 +0200

    Test

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Testar att lägga till filen direkt i Git mappen.");
+		System.out.println("Testar att ha denna filen direkt i eclipse.");
+	}
+
+}
